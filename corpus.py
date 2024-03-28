bf940d7f353f2122ce2456537d327bc3b2b5f857
Can AKSU <can.aksu@ogr.iu.edu.tr>
2024-03-28 18:02:00 +0300

    rewrote some stuff

--- corpus.py
+++ corpus.py
@@ -1,7 +1,84 @@
+import newspaper
+import json
+
+
+def build_source(site_url):
+    source = newspaper.build(site_url, language="tr")
+    return source
+
+
+def download_articles(news_list):
+    newspaper.news_pool.set(news_list, threads_per_source=9)
+    newspaper.news_pool.join()
+
+
+def test_article(article: newspaper.article.Article) -> bool:
+    # download_and_parse_article(article)
+    if article.is_parsed:
+        has_title = isinstance(article.title, str)
+        has_description = isinstance(article.meta_description, str)
+        has_text = isinstance(article.text, str)
+        has_url = isinstance(article.url, str)
+        has_img_url = isinstance(article.meta_img, str)
+        has_date = isinstance(article.meta_data["dateModified"], str)
+        not_media_news = article.is_media_news is False
+
+        return has_title & has_description & has_text &\
+            has_url & has_img_url & has_date & not_media_news &\
+            article.is_valid_body()
+    else:
+        article.parse()
+        test_article(article)
+
+
+def get_metadata(article: newspaper.article.Article, publisher: str) -> dict:
+    article.parse()
+    output = {
+        "title": article.title,
+        "description": article.meta_description,
+        "text": article.text,
+        "url": article.url,
+        "img_url": article.meta_img,
+        "publishing_date": article.meta_data["dateModified"],
+        "publisher": publisher,
+    }
+    return output
 
 
 def main():
-    pass
+    hurriyet = build_source(NEWS_SITES['hurriyet'])
+    sozcu = build_source(NEWS_SITES['sozcu'])
+    milliyet = build_source(NEWS_SITES['milliyet'])
+    haberturk = build_source(NEWS_SITES['haberturk'])
+    yenicag = build_source(NEWS_SITES['yenicag'])
+    cumhuriyet = build_source(NEWS_SITES['cumhuriyet'])
+    haberlercom = build_source(NEWS_SITES['haberler.com'])
+    haber7 = build_source(NEWS_SITES['haber7'])
+    ntv = build_source(NEWS_SITES['ntv'])
+    ensonhaber = build_source(NEWS_SITES['ensonhaber'])
+
+    print("Sources built")
+
+    news_sites = [hurriyet, sozcu, milliyet,
+                  haberturk, yenicag, cumhuriyet,
+                  haberlercom, haber7, ntv,
+                  ensonhaber]
+
+    download_articles(news_sites)
+
+    print("Articles downloaded")
+
+    for source in news_sites:
+        for article in source.articles:
+            if test_article(article):
+                article_data = get_metadata(article,
+                                            f'{source=}'.split('=')[0])
+                with open('./news-data.json', 'a+') as file:
+                    old_data = json.load(file)
+                    old_data.append(article_data)
+                    json.dump(old_data)
+            else:
+                continue
 
 
 # 13 most visited Turkish news sites
@@ -25,6 +102,7 @@ NEWS_SITES = {
     "ntv": "https://www.ntv.com.tr",                  # #12 100/100
     "ensonhaber": "https://www.ensonhaber.com"        # #13 98/100
 }
+
 
 if __name__ == '__main__':
     main()
